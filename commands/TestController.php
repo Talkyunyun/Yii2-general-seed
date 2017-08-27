11ab8ba865b0122af0b953c74c13429c02695392
Gene <talkyunyun@126.com>
2017-08-27 17:40:14 +0800

    重新整理入口文件代码结构

---
+++ commands/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+class TestController extends Controller {
+
+    // 访问: php yii test/index
+    public function actionIndex() {
+        die("sdfsdfdsf");
+    }
+}
